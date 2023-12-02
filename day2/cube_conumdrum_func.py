646334ac24a3c5afe1bbf05f0a665416f5ccca66
Amalie <huishi1885@gmail.com>
2023-12-02 19:05:59 +0000

    Completed the puzzle

--- day2/cube_conumdrum_func.py
+++ day2/cube_conumdrum_func.py
@@ -1,29 +1,52 @@
-
 def color_parser(string):
+    '''
+    parsing a subgame into a list of rgb value
+    :param string: a string describing the sub-game
+    :return: a list of the rgb value
+    '''
     red = 0
     green = 0
-    blue =0
-    cube_catalog = string.split(', ')
+    blue = 0
+    cube_catalog = string.split(", ")
     for cube in cube_catalog:
-        if not cube.find('red')==-1: # if the cube is about the blue kind
-            red += int(cube.lstrip(' ')[0])
-        if not cube.find('green')==-1: # if the cube is about the blue kind
-            green += int(cube.lstrip(' ')[0])
-        if not cube.find('blue')==-1: # if the cube is about the blue kind
-            blue += int(cube.lstrip(' ')[0])
+        if not cube.find("red") == -1:  # if the cube is about the blue kind
+            red += int(cube.split(" ")[0])
+        if not cube.find("green") == -1:  # if the cube is about the blue kind
+            green += int(cube.split(" ")[0])
+        if not cube.find("blue") == -1:  # if the cube is about the blue kind
+            blue += int(cube.split(" ")[0])
     return [red, green, blue]
 
+
 def color_police(game_rgb_value, limit_rgb_value):
-    '''
+    """
     a color police that will alarm (return true) if a cube of any color is out of the limit
-    :param game_rgb_value:
-    :param limit_rgb_value:
+    :param game_rgb_value: the list of the quantities of the cube in the sub-game
+    :param limit_rgb_value: the given limit of the rgb value from the question prompt
     :return:
-    '''
-    red_alarm = game_rgb_value[0] > limit_rgb_value[0]
-    green_alarm = game_rgb_value[1] > limit_rgb_value[1]
-    blue_alarm = game_rgb_value[2] > limit_rgb_value[2]
-    if any([red_alarm, green_alarm, blue_alarm]):
-        return True
-    else:
+    """
+    if game_rgb_value is None:
         return False
+    else:
+        red_alarm = game_rgb_value[0] > limit_rgb_value[0]
+        green_alarm = game_rgb_value[1] > limit_rgb_value[1]
+        blue_alarm = game_rgb_value[2] > limit_rgb_value[2]
+        if any([red_alarm, green_alarm, blue_alarm]):
+            return True
+        else:
+            return False
+
+def max_rgb_for_a_game(rgb_results):
+    """
+    getting the rbg value for a game and produce a list of the maximum number of cube from each color category
+    :param rgb_results: the list of all the sub-game results
+    :return:
+    """
+    red_max = rgb_results[0][0]
+    green_max = rgb_results[0][1]
+    blue_max = rgb_results[0][2]
+    for rgb in rgb_results:
+        red_max = max([red_max, rgb[0]])
+        green_max = max([green_max, rgb[1]])
+        blue_max = max([blue_max, rgb[2]])
+    return [red_max, green_max, blue_max]
--- day2/cube_conundrum.py
+++ day2/cube_conundrum.py
@@ -1,16 +1,17 @@
 import re
-from cube_conumdrum_func import (color_parser, color_police)
+from cube_conumdrum_func import color_parser, color_police, max_rgb_for_a_game
 
 if __name__ == "__main__":
-    data_path = r'../data/day2/example_1.txt'
+    data_path = r"../data/day2/example_2.txt"
     with open(data_path, "r") as f:
         doc = f.readlines()
     red_limit = 12
     green_limit = 13
     blue_limit = 14
     rgb_limit = [red_limit, green_limit, blue_limit]
-    doc = [i.rstrip('\n') for i in doc]
-    game_regex = re.compile(r'''(
+    doc = [i.rstrip("\n") for i in doc]
+    game_regex = re.compile(
+        r"""(
     (Game\s) # 'Game '
     (?P<Game_ID>\d+) # numerical value of the Game ID
     (:\s) # ":" symbol and a space 
@@ -21,16 +22,43 @@ if __name__ == "__main__":
     (?P<Game3>[a-z0-9,\s]*) # (optional) the result of the third game
     (;\s)? # (optional) ';' symbol and a space 
     (?P<Game4>[a-z0-9,\s]*) # (optional) the result of the forth game
-                                 )''', re.VERBOSE)
-    black_list = [] # a list storing all the Game IDs that out of range of the given limit
-    for clue in doc:
+    (;\s)? # (optional) ';' symbol and a space 
+    (?P<Game5>[a-z0-9,\s]*) # (optional) the result of the fifth game
+    (;\s)? # (optional) ';' symbol and a space 
+    (?P<Game6>[a-z0-9,\s]*) # (optional) the result of the sixth game
+                                 )""",
+        re.VERBOSE,
+    )
+    white_list = (
+        []
+    )
+    power_rgb_sum = 0# a list storing all the Game IDs that out of range of the given limit
+    for loop, clue in enumerate(doc):
         mo = game_regex.search(clue)
-        game_id = mo.group('Game_ID')
-        game_1 = mo.group('Game1')
-        game_2 = mo.group('Game2')
-        game_3= mo.group('Game3')
-        game_4 = mo.group('Game4')
-        actual_rgb_value = color_parser(game_1)
-        game_1_status = color_police(actual_rgb_value, rgb_limit)
-
-    a=1
+        game_id = mo.group("Game_ID")
+        game_1 = mo.group("Game1")
+        game_2 = mo.group("Game2")
+        game_3 = mo.group("Game3")
+        game_4 = mo.group("Game4")
+        game_5 = mo.group("Game5")
+        game_6 = mo.group("Game6")
+        actual_rgb_value_game1 = color_parser(game_1)
+        game_1_status = color_police(actual_rgb_value_game1, rgb_limit)
+        actual_rgb_value_game2 = color_parser(game_2)
+        game_2_status = color_police(actual_rgb_value_game2, rgb_limit)
+        actual_rgb_value_game3 = color_parser(game_3)
+        game_3_status = color_police(actual_rgb_value_game3, rgb_limit)
+        actual_rgb_value_game4 = color_parser(game_4)
+        game_4_status = color_police(actual_rgb_value_game4, rgb_limit)
+        actual_rgb_value_game5 = color_parser(game_5)
+        game_5_status = color_police(actual_rgb_value_game5, rgb_limit)
+        actual_rgb_value_game6 = color_parser(game_6)
+        game_6_status = color_police(actual_rgb_value_game6, rgb_limit)
+        rgb_results = [actual_rgb_value_game1, actual_rgb_value_game2, actual_rgb_value_game3, actual_rgb_value_game4, actual_rgb_value_game5, actual_rgb_value_game6]
+        max_rgb = max_rgb_for_a_game(rgb_results)
+        power_rgb = max_rgb[0]*max_rgb[1]*max_rgb[2]
+        power_rgb_sum += power_rgb
+        if not any([game_1_status, game_2_status, game_3_status, game_4_status,game_5_status,game_6_status]):
+            white_list.append(int(game_id))
+    print(sum(white_list))
+    print(power_rgb_sum)
