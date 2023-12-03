16658dc26a72ed77303a28e8dc999c41ad22594b
Amalie <huishi1885@gmail.com>
2023-12-03 08:32:23 +0000

    Getting stuck and giving up temporarily

--- day3/engine_gear_func.py
+++ day3/engine_gear_func.py
@@ -1,16 +1,61 @@
-from itertools import chain
+import re
+
 
 def is_digit(str):
     return [i.isdigit() for i in str]
 
-def is_symbol(str):
-    symbols = '*, #, +, $'
-    symbols = symbols.split(', ')
+def gather_all_symbols(doc):
+    symbols_regex = re.compile(r'([^a-zA-Z0-9\.])')
+    symbols = []
+    for line in doc:
+        symbol = symbols_regex.findall(line)
+        symbols += symbol
+    return set(symbols)
+
+
+
+def is_symbol(str, symbols):
     return [i in symbols for i in str]
 
 
 def find_positions_of_all_true_cell(boolean_2d_list):
-    indices_in_2D = [[]]
+    indices = []
     for row_index, row_value in enumerate(boolean_2d_list):
-        indices =[[row_index, column_index] for column_index, x in enumerate(row_value) if x]
+        for column_index, x in enumerate(row_value):
+            if x:
+                indices.append([row_index, column_index])
     return indices
+
+def is_neighbor_symbol(is_digit_indices, is_symbol_indices):
+    neighbor_indices = []
+    for is_digit_index in is_digit_indices:
+        r = is_digit_index[0]
+        c = is_digit_index[1]
+        for i in range(r-1, r+2):
+            for j in range(c-1,c+2):
+                cell_index = [i, j]
+                if cell_index in is_symbol_indices:
+                    neighbor_indices.append(is_digit_index)
+    return neighbor_indices
+
+def extract_value_from_part_number_index(index, doc):
+    r = index[0]
+    c = index[1]
+    row_schema = list(doc[r])
+    reversed_value = []
+    for i in reversed(row_schema[:c]):
+        if i.isdigit():
+            reversed_value.append(i)
+        else:
+            break
+    start_value = ''.join(reversed(reversed_value))
+    end_value = []
+    for i in row_schema[c:]:
+        if i.isdigit():
+            end_value.append(i)
+        else:
+            break
+    start_value = ''.join(reversed(reversed_value))
+    end_value=''.join(end_value)
+    final_value = start_value+end_value
+    return final_value
--- day3/gondola_lift_engine_gears.py
+++ day3/gondola_lift_engine_gears.py
@@ -1,17 +1,27 @@
-from engine_gear_func import (is_digit, is_symbol, find_positions_of_all_true_cell)
+from engine_gear_func import (is_digit, gather_all_symbols, is_symbol, find_positions_of_all_true_cell,
+                              is_neighbor_symbol, extract_value_from_part_number_index)
 
 if __name__ == '__main__':
-    data_path = r'../data/day3/example_engine_schema1.txt'
+    data_path = r'../data/day3/schema_example2.txt'
 
     with open(data_path, 'r') as f:
         doc = f.readlines()
+    doc = [i.strip() for i in doc]
 
     is_digit_map = []
     is_symbol_map = []
     for row in doc:
         is_digit_for_row = is_digit(row)
-        is_symbol_for_row = is_symbol(row)
+        symbols = gather_all_symbols(doc)
+        is_symbol_for_row = is_symbol(row, symbols)
         is_digit_map.append(is_digit_for_row)
         is_symbol_map.append(is_symbol_for_row)
-        a = find_positions_of_all_true_cell(is_digit_map)
-    a=1
+    is_digit_indices = find_positions_of_all_true_cell(is_digit_map)
+    is_symbol_indices = find_positions_of_all_true_cell(is_symbol_map)
+    part_number_indices = is_neighbor_symbol(is_digit_indices, is_symbol_indices)
+    all_part_numbers = []
+    for part_number_index in part_number_indices:
+        part_number = extract_value_from_part_number_index(part_number_index, doc)
+        all_part_numbers.append(int(part_number))
+    all_part_numbers = set(all_part_numbers)
+    print(sum(all_part_numbers))
