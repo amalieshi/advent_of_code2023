9db8ffb89bf80bdc257a4b5528882f7a246f32bb
Amalie <huishi1885@gmail.com>
2023-12-05 22:02:22 +0000

    Part 1 works for the example, and I royally give up for this terrible question

--- day5/main.py
+++ day5/main.py
@@ -1,11 +1,11 @@
 from farming_func import break_down_almanac, produce_destination_list
 import re
+import numpy as np
 import pandas as pd
-# TODO 3: Find the location of the see.d
 # TODO 4: Get the minimum seed number.
 
 if __name__ == '__main__':
-    data_path = '../data/day5/almanac1.txt'
+    data_path = '../data/day5/almanac2.txt'
     with open(data_path,'r') as f:
         doc = f.readlines()
     # Step 1: Break down the list into a dictionary.
@@ -15,13 +15,26 @@ if __name__ == '__main__':
     seeds_value = [int(i) for i in seeds_value]
     almanac_maps = doc[2:]
     almanac_maps = break_down_almanac(almanac_maps)
-    # Create an panda dataframe of source and the corresponding destination.
+    # Create a panda dataframe of source and the corresponding destination.
     full_list = []
     for key in almanac_maps.keys():
-        list = produce_destination_list(almanac_maps[key])
-        full_list.append(list)
+        lst = produce_destination_list(almanac_maps[key])
+        full_list.append(lst)
     df = pd.DataFrame(full_list)
     df = df.T
     column_names = [i for i in almanac_maps.keys()]
     df.columns = column_names
-    a=1
+    # Find the location of the seed
+    total_steps = len(df.columns)
+    all_locations = []
+    for tracker in seeds_value:
+        for i in range(total_steps):
+            series = df.iloc[:, i]
+            result = series.iloc[tracker]
+            if np.isnan(result):
+                result = int(tracker)
+            else:
+                tracker = int(result)
+        all_locations.append(tracker)
+    answer_to_part_one = min(all_locations)
+    print(f"The answer to part one is {answer_to_part_one}.")
