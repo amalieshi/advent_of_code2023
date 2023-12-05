d3b402dd3bce2b6b38dd0c17d08e7759e13d2d81
Amalie <huishi1885@gmail.com>
2023-12-05 20:47:41 +0000

    Created a easily visualized data frame that seems to work

--- day5/farming_func.py
+++ day5/farming_func.py
@@ -15,7 +15,7 @@ def break_down_almanac(almanac):
         key, values = re.split(r'\:', category)
         values = re.split(r'\n', values)
         values = [i for i in values if i != '']
-        values = [re.split('\s', i) for i in values]
+        values = [re.split(r'\s', i) for i in values]
         for loop, value in enumerate(values):
             value = [int(i) for i in value if i != '' ]
             values[loop] = value
@@ -45,15 +45,16 @@ def find_max_end_value(lst):
     max_end_value = max([destination_end, source_end])
     return max_end_value
 
-def produce_destination_series(almanac_2D_list):
+def produce_destination_list(almanac_2D_list):
     max_value = 0
     for i in almanac_2D_list:
         max_in_one_item = find_max_end_value(i)
         if max_in_one_item > max_value:
             max_value = max_in_one_item
     destination_lst = range(0, max_value)
-    desination_series =pd.Series(destination_lst)
+    index_lst = [str(i) for i in destination_lst]
+    desination_series =pd.Series(destination_lst, index=index_lst)
     for i in almanac_2D_list:
-        swap_location, swap_value = find_swapping_location_and_value()
-        # desination_series.iloc(swap_location) = swap_value
-
+        swap_location, swap_value = find_swapping_location_and_value(i)
+        desination_series.iloc[swap_location] = swap_value
+    return desination_series.to_list()
--- day5/main.py
+++ day5/main.py
@@ -1,7 +1,6 @@
-from farming_func import break_down_almanac, produce_destination_series
+from farming_func import break_down_almanac, produce_destination_list
 import re
 import pandas as pd
-# TODO 2: Create an panda dataframe of source and the corresponding destination.
 # TODO 3: Find the location of the see.d
 # TODO 4: Get the minimum seed number.
 
@@ -11,10 +10,18 @@ if __name__ == '__main__':
         doc = f.readlines()
     # Step 1: Break down the list into a dictionary.
     seeds = doc[0].replace('\n','')
-    _ , seeds_value = seeds.split(': ')
-    seeds_value = re.split('\s', seeds_value)
+    _ , seeds_value = seeds.split(r': ')
+    seeds_value = re.split(r'\s', seeds_value)
     seeds_value = [int(i) for i in seeds_value]
     almanac_maps = doc[2:]
     almanac_maps = break_down_almanac(almanac_maps)
-    produce_destination_series(almanac_maps['seed-to-soil'])
+    # Create an panda dataframe of source and the corresponding destination.
+    full_list = []
+    for key in almanac_maps.keys():
+        list = produce_destination_list(almanac_maps[key])
+        full_list.append(list)
+    df = pd.DataFrame(full_list)
+    df = df.T
+    column_names = [i for i in almanac_maps.keys()]
+    df.columns = column_names
     a=1
